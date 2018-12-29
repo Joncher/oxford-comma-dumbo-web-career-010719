6a69d2b80ebc1ceed8f2a92d83e56d5600fa5f39
John Crespi Oncher <johnoncher@gmail.com>
2018-12-29 16:40:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -15,7 +15,9 @@ def oxford_comma(array)
   
   if array.size > 3
     
-    #/ array[array.size-2] << " and "/#
-    return array.join(", ")
+    array2 = []
+    array2 = array.join(", ")
+    array2[array2.size-2] << " and "
+    return array2
   end
 end

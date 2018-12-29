de7d04eef08bd7ec754abe6822f301bcb714740c
John Crespi Oncher <johnoncher@gmail.com>
2018-12-29 16:11:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -13,9 +13,10 @@ def oxford_comma(array)
     return array.join
   end
   
-  if array.size == 3
-  array[0] << ", "
-  array[1] << ", and "
-  return array.join
+  if array.size == 4
+    array[0] << ", "
+    array[1] << ", "
+    array[2] << ", and "
+    return array.join
   end
 end
